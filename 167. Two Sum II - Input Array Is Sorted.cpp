85eb7fe1dbad81509910f77c6b4a8f5aa7ab82a3
Raj kumar <rajkumar.rk0104@gmail.com>
2025-04-17 18:46:56 +0530

    two Sum II - Input Array Is Sorted. solved using 2 pointer
    https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/?envType=study-plan-v2&envId=top-interview-150

---
+++ 167. Two Sum II - Input Array Is Sorted.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int> result;
+
+        // using 2 pointer
+        int left = 0;       //start pointer
+        int right = numbers.size() - 1; //end pointer
+
+    // using 2 pointer
+        while (left < right) {
+            int result = numbers[left] + numbers[right];
+
+            if (target == result) {
+                return {left + 1, right + 1}; // as indexing starts from 1 not 0
+            } 
+            
+            else if (result < target) {
+                left++; // as array is sorted, so we move forward
+            } 
+            else {
+                right--;
+            }
+        }
+        return {}; //return blank if nothing found
+    }
+};
