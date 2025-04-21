120e49d32d208bc86140888bc9d08d1662c8c0ac
Raj kumar <rajkumar.rk0104@gmail.com>
2025-04-21 15:47:46 +0530

    Solved using 2 binary search
    One for the left half and another for the right half side of the array.

---
+++ 33. Search in Rotated Sorted Array.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+    // use 2 binary search, 1 on the left sorted array and another on the right sorted array.
+
+        int left = 0, right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) return mid;
+
+            // Left half is sorted
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid])
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+            }
+            // Right half is sorted
+            else {
+                if (nums[mid] < target && target <= nums[right])
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+};
