1da5475de4e68be2aac188038368d8737d89e77c
Raj kumar <rajkumar.rk0104@gmail.com>
2025-12-17 00:47:06 +0530

    Add comments to clarify missing number approach
    Added comments explaining the approach to find the missing number.
    
    https://leetcode.com/problems/missing-number/

--- 268. MissingNumber.cpp
+++ 268. MissingNumber.cpp
@@ -1,3 +1,11 @@
+//NOTE : IT ONLY WORKS WHEN 1 DIGIT IS MISSING.
+/*
+Approach:
+- find the expectedSum using the math formula --> n(n+1)/2
+- find the actualSum --> using the loop
+- return expectedSum - actualSum
+*/
+
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
