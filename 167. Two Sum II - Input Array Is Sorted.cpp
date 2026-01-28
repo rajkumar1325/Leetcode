8a8554fabddb8e09fae818af77d41c1f44aecf03
Raj kumar <rajkumar.rk0104@gmail.com>
2026-01-28 10:58:32 +0530

    twoSum function with improved pointer logic
    Refactored twoSum function to improve readability and maintain functionality.
    
    https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/solutions/7530781/167-two-sum-ii-input-array-is-sorted-eff-avy5/

--- 167. Two Sum II - Input Array Is Sorted.cpp
+++ 167. Two Sum II - Input Array Is Sorted.cpp
@@ -2,25 +2,18 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
 
-        // Assigning 2 pointer
-        int left = 0;       //start pointer
-        int right = numbers.size() - 1; //end pointer
-
-    // using 2 pointer
-        while (left < right) {
-            int result = numbers[left] + numbers[right];
-
-            if (target == result) {
-                return {left + 1, right + 1}; // as indexing starts from 1 not 0
-            } 
-            
-            else if (result < target) {
-                left++; // as array is sorted, so we move forward
-            } 
-            else {
-                right--;
+        // using 2 pointer
+        int l= 0;
+        int r = numbers.size()-1; 
+        
+        while(l<r){
+            if(numbers[l] + numbers[r] == target){
+                return {l+1, r+1}; // As it want 1 based indx. return as a vector of list
             }
+
+            else if( numbers[l] + numbers[r] < target ) l++; //hve to make our value big
+            else r--;
         }
-        return {}; //return blank if nothing found
+        return {-1, -1}; //leetcode safe
     }
 };
